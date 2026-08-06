71ddbd725fce49a66b9f1f9ef5f68e8299cfd0db
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-06 14:41:28 +0700

    [update] BindJSON for request

---
+++ request.go
@@ -0,0 +1,21 @@
+package gohan
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func BindJSON(r *http.Request, target interface{}) error {
+	if r.Body == nil {
+		return fmt.Errorf("request body empty")
+	}
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(target); err != nil {
+		return fmt.Errorf("invalid JSON payload: %w", err)
+	}
+
+	return nil
+}
