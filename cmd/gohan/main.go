047e8a23b8d2e67676482161264c6bef8305a04c
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-07 08:33:42 +0700

    [framework] main template

--- cmd/gohan/main.go
+++ cmd/gohan/main.go
@@ -7,14 +7,37 @@ import (
 
 const routesTemplate = `package main
 
-import "github.com/farkhanisturkia/gohan"
+import (
+	"net/http"
 
-func SetupRoutes(app *gohan.Gohan) {
-	app.Get("/ping", func(req *gohan.Request, res *gohan.Response) {
-		res.JSON(200, map[string]string{
+	"github.com/farkhanisturkia/gohan"
+)
+
+func SetupRoutes() {
+	gohan.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		gohan.JSON(w, http.StatusOK, map[string]string{
 			"message": "pong",
 		})
 	})
+}
+`
+
+const mainTemplate = `package main
+
+import (
+	"log"
+
+	"github.com/farkhanisturkia/gohan"
+)
+
+func main() {
+	env := gohan.GetEnv()
+
+	SetupRoutes()
+
+	if err := gohan.Serve(&env.AppPort); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
 }
 `
 
@@ -28,11 +51,29 @@ func main() {
 
 	switch command {
 	case "init":
+		createMainFile()
 		createRoutesFile()
 	default:
 		fmt.Printf("The command '%s' is not recognized.\n", command)
 		printHelp()
 	}
+}
+
+func createMainFile() {
+	filePath := "main.go"
+
+	if _, err := os.Stat(filePath); err == nil {
+		fmt.Println("[info] The 'main.go' file already exists in this directory.")
+		return
+	}
+
+	err := os.WriteFile(filePath, []byte(mainTemplate), 0644)
+	if err != nil {
+		fmt.Printf("[error] Failed to create the 'main.go' file: %v\n", err)
+		return
+	}
+
+	fmt.Println("[info] The 'main.go' file was successfully created!")
 }
 
 func createRoutesFile() {
