ec10f54b773924fc3e44a81b6775a7e2445dc4a8
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-07 13:09:19 +0700

    [framework v1.1.6] show version and help usage

--- cmd/gohan/main.go
+++ cmd/gohan/main.go
@@ -7,24 +7,44 @@ import (
 	"github.com/farkhanisturkia/gohan/cmd/gohan/commands"
 )
 
+const AppVersion = "v1.1.6"
+
 func main() {
 	if len(os.Args) < 2 {
 		printHelp()
 		return
 	}
 
-	command := os.Args[1]
-	switch command {
+	arg := os.Args[1]
+
+	switch arg {
 	case "init":
 		commands.InitBoilerplate()
-	default:
-		fmt.Printf("The command '%s' is not recognized.\n", command)
+
+	case "-v", "--version":
+		printVersion()
+
+	case "-h", "--help":
 		printHelp()
+
+	default:
+		fmt.Printf("[error] The command '%s' is not recognized.\n", arg)
+		printHelp()
+		os.Exit(1)
 	}
 }
 
+func printVersion() {
+	fmt.Printf("Gohan CLI version %s\n", AppVersion)
+}
+
 func printHelp() {
-	fmt.Println("Gohan Framework CLI")
+	fmt.Println("Gohan Framework CLI Generator")
 	fmt.Println("\nUsage:")
-	fmt.Println("  gohan init   : Generate standard Gohan Framework")
+	fmt.Println("  gohan <command>/<flags>")
+	fmt.Println("\nAvailable Commands:")
+	fmt.Println("  init          Generate standard Gohan Framework")
+	fmt.Println("\nFlags:")
+	fmt.Println("  -v, --version Show the CLI version")
+	fmt.Println("  -h, --help    Display the CLI usage instructions")
 }
