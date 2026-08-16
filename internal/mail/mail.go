d6985e28e68e6babcb378936cba8ac087e32c8a8
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-17 05:39:50 +0700

    [gohan] gohan sendmail

---
+++ internal/mail/mail.go
@@ -0,0 +1,94 @@
+package mail
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/smtp"
+	"strconv"
+
+	"github.com/farkhanisturkia/gohan/internal/config"
+)
+
+func SendEmail(to string, subject string, body string) error {
+	env := config.GetEnv()
+
+	// Jika MAIL_DRIVER diset "log", cukup log ke console (cocok untuk dev local)
+	if env.MailDriver == "log" || env.MailHost == "" {
+		fmt.Printf("[MAIL LOG] To: %s | Subject: %s | Body: %s\n", to, subject, body)
+		return nil
+	}
+
+	from := env.MailFromAddress
+	fromName := env.MailFromName
+	if fromName == "" {
+		fromName = env.AppName
+	}
+
+	headers := make(map[string]string)
+	headers["From"] = fmt.Sprintf("%s <%s>", fromName, from)
+	headers["To"] = to
+	headers["Subject"] = subject
+	headers["MIME-Version"] = "1.0"
+	headers["Content-Type"] = "text/html; charset=UTF-8"
+
+	message := ""
+	for k, v := range headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + body
+
+	addr := fmt.Sprintf("%s:%s", env.MailHost, env.MailPort)
+	host := env.MailHost
+
+	var auth smtp.Auth
+	if env.MailUsername != "" {
+		auth = smtp.PlainAuth("", env.MailUsername, env.MailPassword, host)
+	}
+
+	portInt, _ := strconv.Atoi(env.MailPort)
+
+	// Koneksi SSL (Port 465) vs STARTTLS / Non-SSL (Port 587 / 25 / 2525)
+	if env.MailEncryption == "ssl" || portInt == 465 {
+		tlsconfig := &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         host,
+		}
+
+		conn, err := tls.Dial("tcp", addr, tlsconfig)
+		if err != nil {
+			return fmt.Errorf("failed to connect SSL TLS: %w", err)
+		}
+
+		client, err := smtp.NewClient(conn, host)
+		if err != nil {
+			return fmt.Errorf("failed to create SMTP client: %w", err)
+		}
+		defer client.Close()
+
+		if auth != nil {
+			if err = client.Auth(auth); err != nil {
+				return fmt.Errorf("SMTP auth failed: %w", err)
+			}
+		}
+
+		if err = client.Mail(from); err != nil {
+			return err
+		}
+		if err = client.Rcpt(to); err != nil {
+			return err
+		}
+
+		w, err := client.Data()
+		if err != nil {
+			return err
+		}
+		_, err = w.Write([]byte(message))
+		if err != nil {
+			return err
+		}
+		return w.Close()
+	}
+
+	// Default: Standard SMTP / STARTTLS
+	return smtp.SendMail(addr, auth, from, []string{to}, []byte(message))
+}
