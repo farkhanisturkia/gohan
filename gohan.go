6e6c9608a4c38011f554da5d77101021b89fa3c1
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-16 17:19:02 +0700

    [gohan] export function and type

---
+++ gohan.go
@@ -0,0 +1,47 @@
+package gohan
+
+import (
+	"github.com/farkhanisturkia/gohan/internal/config"
+	"github.com/farkhanisturkia/gohan/internal/database"
+	gohanHttp "github.com/farkhanisturkia/gohan/internal/http"
+	"github.com/farkhanisturkia/gohan/internal/security"
+	"github.com/farkhanisturkia/gohan/utils"
+)
+
+// Config
+type Env = config.Env
+
+var InitEnv = config.InitEnv
+var GetEnv = config.GetEnv
+
+// Database
+type DB = database.DB
+
+var GetConn = database.GetConn
+
+// HTTP Request & Response
+var BindJSON = gohanHttp.BindJSON
+var JSON = gohanHttp.JSON
+var Error = gohanHttp.Error
+var Param = gohanHttp.Param
+
+// HTTP Router & Server
+type Router = gohanHttp.Router
+
+var SetRoute = gohanHttp.SetRoute
+var Get = gohanHttp.Get
+var Post = gohanHttp.Post
+var Put = gohanHttp.Put
+var Patch = gohanHttp.Patch
+var Delete = gohanHttp.Delete
+var Serve = gohanHttp.Serve
+
+// Security
+var HashPassword = security.HashPassword
+var CheckPasswordHash = security.CheckPasswordHash
+var GenerateRandomToken = security.GenerateRandomToken
+var HashToken = security.HashToken
+
+// Utils
+var GetClientIP = utils.GetClientIP
+var ParseTokenName = utils.ParseTokenName
