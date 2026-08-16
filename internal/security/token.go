cfb793e77c3e883c19b9d955680fa8380c94235e
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-17 05:42:15 +0700

    [gohan] GenerateRandomToken length param

--- internal/security/token.go
+++ internal/security/token.go
@@ -9,8 +9,8 @@ import (
 	"github.com/farkhanisturkia/gohan/internal/config"
 )
 
-func GenerateRandomToken() (string, error) {
-	bytes := make([]byte, 32)
+func GenerateRandomToken(length int) (string, error) {
+	bytes := make([]byte, length)
 	if _, err := rand.Read(bytes); err != nil {
 		return "", err
 	}
