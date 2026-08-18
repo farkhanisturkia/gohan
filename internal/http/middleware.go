a838db55f02b617af854dc65b815852e411d3104
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-18 12:42:37 +0700

    [gohan] timeout middleware duration

---
+++ internal/http/middleware.go
@@ -0,0 +1,16 @@
+package http
+
+import (
+    "net/http"
+    "time"
+)
+
+func TimeoutMiddleware(timeout time.Duration) func(http.Handler) http.Handler {
+    return func(next http.Handler) http.Handler {
+        return http.TimeoutHandler(
+            next,
+            timeout,
+            `{"message":"Request timeout"}`,
+        )
+    }
+}
