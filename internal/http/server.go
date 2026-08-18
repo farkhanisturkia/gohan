98ebc45f7cc8dd75e792668a291f3eefec41a42f
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-18 12:46:46 +0700

    [gohan] serve with add use middleware setting up

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,41 +1,58 @@
 package http
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strings"
+    "fmt"
+    "log"
+    "net/http"
+    "strings"
 )
 
+type MiddlewareFunc func(http.Handler) http.Handler
+
+var globalMiddlewares []MiddlewareFunc
+
+func Use(mw ...MiddlewareFunc) {
+    globalMiddlewares = append(globalMiddlewares, mw...)
+}
+
+func chainMiddlewares(handler http.Handler) http.Handler {
+    for i := len(globalMiddlewares) - 1; i >= 0; i-- {
+        handler = globalMiddlewares[i](handler)
+    }
+    return handler
+}
+
 func Serve(port interface{}) error {
-	var portStr string
+    var portStr string
 
-	switch v := port.(type) {
-	case string:
-		portStr = v
-	case *string:
-		if v != nil {
-			portStr = *v
-		} else {
-			portStr = "8080"
-		}
-	default:
-		portStr = fmt.Sprintf("%v", port)
-	}
+    switch v := port.(type) {
+    case string:
+        portStr = v
+    case *string:
+        if v != nil {
+            portStr = *v
+        } else {
+            portStr = "8080"
+        }
+    default:
+        portStr = fmt.Sprintf("%v", port)
+    }
 
-	if !strings.HasPrefix(portStr, ":") {
-		portStr = ":" + portStr
-	}
+    if !strings.HasPrefix(portStr, ":") {
+        portStr = ":" + portStr
+    }
 
-	if len(defaultRouter.routes) == 0 {
-		SetRoute("GET", "/", func(w http.ResponseWriter, r *http.Request) {
-			JSON(w, http.StatusOK, map[string]string{
-				"message": "Welcome to Gohan Framework!",
-			})
-		})
-	}
+    if len(defaultRouter.routes) == 0 {
+        SetRoute("GET", "/", func(w http.ResponseWriter, r *http.Request) {
+            JSON(w, http.StatusOK, map[string]string{
+                "message": "Welcome to Gohan Framework!",
+            })
+        })
+    }
 
-	log.Printf("[info] Server running in http://localhost%s\n", portStr)
+    log.Printf("[info] Server running in http://localhost%s\n", portStr)
 
-	return http.ListenAndServe(portStr, defaultRouter)
+    handler := chainMiddlewares(defaultRouter)
+
+    return http.ListenAndServe(portStr, handler)
 }
