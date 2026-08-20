4e6d0b77f05ad136c1131e1e3320e14d07f22e8c
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-20 08:37:02 +0700

    [gohan] get module name using cut prefix

--- cmd/gohan/utils/helper.go
+++ cmd/gohan/utils/helper.go
@@ -1,24 +1,28 @@
 package utils
 
 import (
-	"bufio"
-	"os"
-	"strings"
+    "bufio"
+    "os"
+    "strings"
 )
 
 func GetModuleName() string {
-	file, err := os.Open("go.mod")
-	if err != nil {
-		return "myproject"
-	}
-	defer file.Close()
+    file, err := os.Open("go.mod")
+    if err != nil {
+        return "myproject"
+    }
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if strings.HasPrefix(line, "module ") {
-			return strings.TrimPrefix(line, "module ")
-		}
-	}
-	return "myproject"
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := strings.TrimSpace(scanner.Text())
+        if moduleName, found := strings.CutPrefix(line, "module "); found {
+            moduleName = strings.TrimSpace(moduleName)
+            moduleName = strings.Trim(moduleName, `"'`)
+            if moduleName != "" {
+                return moduleName
+            }
+        }
+    }
+    return "myproject"
 }
