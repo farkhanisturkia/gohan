805de5a9d2aeb2ae13badac7cf5f479058b8dfaf
farkhanisturkia <farkhan22032000@gmail.com>
2026-08-20 08:45:24 +0700

    [framework v1.4.5] fixing make command and auto set to default.go

--- cmd/gohan/commands/make.go
+++ cmd/gohan/commands/make.go
@@ -1,136 +1,169 @@
 package commands
 
 import (
-	"bytes"
-	"fmt"
-	"go/format"
-	"os"
-	"path/filepath"
-	"strings"
-	"text/template"
-	"time"
+    "fmt"
+    "go/format"
+    "os"
+    "path/filepath"
+    "strings"
+    "time"
 
-	"github.com/farkhanisturkia/gohan/cmd/gohan/templates"
-	"github.com/farkhanisturkia/gohan/cmd/gohan/utils"
+    "github.com/farkhanisturkia/gohan/cmd/gohan/templates"
+    "github.com/farkhanisturkia/gohan/cmd/gohan/utils"
 )
 
-type TemplateData struct {
-	ModuleName string
-	Prefix     string
-	Name       string
-}
-
 func toPascalCase(s string) string {
-	s = strings.TrimSuffix(s, ".go")
-	s = strings.TrimPrefix(s, "create_")
-	s = strings.TrimSuffix(s, "_controller")
-	s = strings.TrimSuffix(s, "_seeder")
-	s = strings.TrimSuffix(s, "_migration")
-	s = strings.TrimSuffix(s, "_table")
+    s = strings.TrimSuffix(s, ".go")
+    s = strings.TrimPrefix(s, "create_")
+    s = strings.TrimSuffix(s, "_controller")
+    s = strings.TrimSuffix(s, "_seeder")
+    s = strings.TrimSuffix(s, "_migration")
+    s = strings.TrimSuffix(s, "_table")
 
-	words := strings.FieldsFunc(s, func(r rune) bool {
-		return r == '_' || r == '-' || r == ' '
-	})
+    words := strings.FieldsFunc(s, func(r rune) bool {
+        return r == '_' || r == '-' || r == ' '
+    })
 
-	for i, w := range words {
-		if len(w) > 0 {
-			words[i] = strings.ToUpper(w[:1]) + w[1:]
-		}
-	}
-	return strings.Join(words, "")
+    for i, w := range words {
+        if len(w) > 0 {
+            words[i] = strings.ToUpper(w[:1]) + w[1:]
+        }
+    }
+    return strings.Join(words, "")
 }
 
-func generateFromTemplate(tmplFileName, targetPath string, data TemplateData) {
-	tmplContentStr, err := templates.GetMakeTemplate(tmplFileName)
-	if err != nil {
-		fmt.Printf("[error] %v\n", err)
-		return
-	}
+func generateFromTemplate(tmplFileName, targetPath string, data templates.MakeData) {
+    renderedContent, err := templates.RenderMakeTemplate(tmplFileName, data)
+    if err != nil {
+        fmt.Printf("[error] %v\n", err)
+        return
+    }
 
-	tmpl, err := template.New("make").Parse(tmplContentStr)
-	if err != nil {
-		fmt.Printf("[error] Failed to parse template: %v\n", err)
-		return
-	}
+    dir := filepath.Dir(targetPath)
+    if dir != "." {
+        if err := os.MkdirAll(dir, 0755); err != nil {
+            fmt.Printf("[error] Failed to create directory %s: %v\n", dir, err)
+            return
+        }
+    }
 
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, data); err != nil {
-		fmt.Printf("[error] Failed to render template: %v\n", err)
-		return
-	}
+    if _, err := os.Stat(targetPath); !os.IsNotExist(err) {
+        fmt.Printf("[error] The file %s already exists\n", targetPath)
+        return
+    }
 
-	dir := filepath.Dir(targetPath)
-	if dir != "." {
-		if err := os.MkdirAll(dir, 0755); err != nil {
-			fmt.Printf("[error] Failed to create directory %s: %v\n", dir, err)
-			return
-		}
-	}
+    fileBytes := []byte(renderedContent)
 
-	if _, err := os.Stat(targetPath); !os.IsNotExist(err) {
-		fmt.Printf("[error] The file %s already exists\n", targetPath)
-		return
-	}
+    if strings.HasSuffix(targetPath, ".go") {
+        formatted, err := format.Source(fileBytes)
+        if err == nil {
+            fileBytes = formatted
+        }
+    }
 
-	fileBytes := buf.Bytes()
+    if err := os.WriteFile(targetPath, fileBytes, 0644); err != nil {
+        fmt.Printf("[error] Failed to create file %s: %v\n", targetPath, err)
+        return
+    }
 
-	if strings.HasSuffix(targetPath, ".go") {
-		formatted, err := format.Source(fileBytes)
-		if err == nil {
-			fileBytes = formatted
-		}
-	}
-
-	if err := os.WriteFile(targetPath, fileBytes, 0644); err != nil {
-		fmt.Printf("[error] Failed to create file %s: %v\n", targetPath, err)
-		return
-	}
-
-	fmt.Printf("[info] Created: %s\n", targetPath)
+    fmt.Printf("[info] Created: %s\n", targetPath)
 }
 
 func MakeController(name string) {
-	cleanName := strings.TrimSuffix(name, ".go")
-	prefix := toPascalCase(cleanName)
-	targetPath := filepath.Join("controllers", cleanName+".go")
-	moduleName := utils.GetModuleName()
+    cleanName := strings.TrimSuffix(name, ".go")
+    prefix := toPascalCase(cleanName)
+    targetPath := filepath.Join("controllers", cleanName+".go")
+    moduleName := utils.GetModuleName()
 
-	data := TemplateData{
-		ModuleName: moduleName,
-		Prefix:     prefix,
-		Name:       cleanName,
-	}
+    data := templates.MakeData{
+        ModuleName: moduleName,
+        Prefix:     prefix,
+    }
 
-	generateFromTemplate("controller.go.tmpl", targetPath, data)
+    generateFromTemplate("controller.go.tmpl", targetPath, data)
 }
 
 func MakeMigration(name string) {
-	cleanName := strings.TrimSuffix(name, ".go")
-	prefix := toPascalCase(cleanName)
-	timestamp := time.Now().Format("20060102150405")
-	targetPath := filepath.Join("database", "migrations", fmt.Sprintf("%s_%s.go", timestamp, cleanName))
-	moduleName := utils.GetModuleName()
+    cleanName := strings.TrimSuffix(name, ".go")
+    prefix := toPascalCase(cleanName)
+    timestamp := time.Now().Format("20060102150405")
 
-	data := TemplateData{
-		ModuleName: moduleName,
-		Prefix:     prefix,
-		Name:       cleanName,
-	}
+    fileName := fmt.Sprintf("%s_%s.go", timestamp, cleanName)
+    targetPath := filepath.Join("database", "migrations", fileName)
+    moduleName := utils.GetModuleName()
 
-	generateFromTemplate("migration.go.tmpl", targetPath, data)
+    data := templates.MakeData{
+        ModuleName: moduleName,
+        Prefix:     prefix,
+    }
+
+    generateFromTemplate("migration.go.tmpl", targetPath, data)
+
+    appendMigrationToDefault(prefix)
 }
 
 func MakeSeeder(name string) {
-	cleanName := strings.TrimSuffix(name, ".go")
-	prefix := toPascalCase(cleanName)
-	targetPath := filepath.Join("database", "seeders", strings.ToLower(prefix)+".go")
-	moduleName := utils.GetModuleName()
+    cleanName := strings.TrimSuffix(name, ".go")
+    prefix := toPascalCase(cleanName)
+    targetPath := filepath.Join("database", "seeders", strings.ToLower(prefix)+".go")
+    moduleName := utils.GetModuleName()
 
-	data := TemplateData{
-		ModuleName: moduleName,
-		Prefix:     prefix,
-		Name:       cleanName,
-	}
+    data := templates.MakeData{
+        ModuleName: moduleName,
+        Prefix:     prefix,
+    }
 
-	generateFromTemplate("seeder.go.tmpl", targetPath, data)
+    generateFromTemplate("seeder.go.tmpl", targetPath, data)
+
+    appendSeederToDefault(prefix)
+}
+
+func appendMigrationToDefault(prefix string) {
+    defaultPath := filepath.Join("database", "migrations", "default.go")
+    content, err := os.ReadFile(defaultPath)
+    if err != nil {
+        return
+    }
+
+    targetCall := fmt.Sprintf("%sMigration(db)", prefix)
+    if strings.Contains(string(content), targetCall) {
+        return
+    }
+
+    updated := strings.Replace(
+        string(content),
+        "}",
+        fmt.Sprintf("\t%s\n}", targetCall),
+        1,
+    )
+
+    formatted, err := format.Source([]byte(updated))
+    if err == nil {
+        _ = os.WriteFile(defaultPath, formatted, 0644)
+    }
+}
+
+func appendSeederToDefault(prefix string) {
+    defaultPath := filepath.Join("database", "seeders", "default.go")
+    content, err := os.ReadFile(defaultPath)
+    if err != nil {
+        return
+    }
+
+    targetCall := fmt.Sprintf("%sSeeder(db)", prefix)
+    if strings.Contains(string(content), targetCall) {
+        return
+    }
+
+    updated := strings.Replace(
+        string(content),
+        "}",
+        fmt.Sprintf("\t%s\n}", targetCall),
+        1,
+    )
+
+    formatted, err := format.Source([]byte(updated))
+    if err == nil {
+        _ = os.WriteFile(defaultPath, formatted, 0644)
+    }
 }
--- cmd/gohan/templates/embed.go
+++ cmd/gohan/templates/embed.go
@@ -1,88 +1,105 @@
 package templates
 
 import (
-	"bytes"
-	"embed"
-	"fmt"
-	"io/fs"
-	"path/filepath"
-	"strings"
-	"text/template"
+    "bytes"
+    "embed"
+    "fmt"
+    "io/fs"
+    "path/filepath"
+    "strings"
+    "text/template"
 )
 
 //go:embed files
 var templateFS embed.FS
 
 type TemplateData struct {
-	ModuleName        string
-	UseAuth           bool
-	AuthType          string
-	UseForgotPassword bool
+    ModuleName        string
+    UseAuth           bool
+    AuthType          string
+    UseForgotPassword bool
 }
 
 func GetBoilerplateTemplates(moduleName string, useAuth bool, authType string, useForgotPassword bool) (map[string]string, error) {
-	result := make(map[string]string)
-	data := TemplateData{
-		ModuleName:        moduleName,
-		UseAuth:           useAuth,
-		AuthType:          authType,
-		UseForgotPassword: useForgotPassword,
-	}
+    result := make(map[string]string)
+    data := TemplateData{
+        ModuleName:        moduleName,
+        UseAuth:           useAuth,
+        AuthType:          authType,
+        UseForgotPassword: useForgotPassword,
+    }
 
-	err := fs.WalkDir(templateFS, "files", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
+    err := fs.WalkDir(templateFS, "files", func(path string, d fs.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
 
-		if d.IsDir() {
-			return nil
-		}
+        if d.IsDir() {
+            return nil
+        }
 
-		content, err := templateFS.ReadFile(path)
-		if err != nil {
-			return fmt.Errorf("failed to read embedded file %s: %w", path, err)
-		}
+        content, err := templateFS.ReadFile(path)
+        if err != nil {
+            return fmt.Errorf("failed to read embedded file %s: %w", path, err)
+        }
 
-		tmpl, err := template.New(d.Name()).Parse(string(content))
-		if err != nil {
-			return fmt.Errorf("failed to parse template %s: %w", path, err)
-		}
+        tmpl, err := template.New(d.Name()).Parse(string(content))
+        if err != nil {
+            return fmt.Errorf("failed to parse template %s: %w", path, err)
+        }
 
-		var buf bytes.Buffer
-		if err := tmpl.Execute(&buf, data); err != nil {
-			return fmt.Errorf("failed to execute template %s: %w", path, err)
-		}
+        var buf bytes.Buffer
+        if err := tmpl.Execute(&buf, data); err != nil {
+            return fmt.Errorf("failed to execute template %s: %w", path, err)
+        }
 
-		relPath, err := filepath.Rel("files", path)
-		if err != nil {
-			return err
-		}
+        relPath, err := filepath.Rel("files", path)
+        if err != nil {
+            return err
+        }
 
-		slashPath := filepath.ToSlash(relPath)
-		targetPath := strings.TrimSuffix(slashPath, ".tmpl")
+        slashPath := filepath.ToSlash(relPath)
+        targetPath := strings.TrimSuffix(slashPath, ".tmpl")
 
-		if targetPath == "env" {
-			targetPath = ".env"
-		}
+        if targetPath == "env" {
+            targetPath = ".env"
+        }
 
-		result[targetPath] = buf.String()
-		return nil
-	})
+        result[targetPath] = buf.String()
+        return nil
+    })
 
-	if err != nil {
-		return nil, fmt.Errorf("failed to process boilerplate templates: %w", err)
-	}
+    if err != nil {
+        return nil, fmt.Errorf("failed to process boilerplate templates: %w", err)
+    }
 
-	return result, nil
+    return result, nil
 }
 
-//go:embed makes/*.tmpl
+//go:embed makes
 var MakeFS embed.FS
 
-func GetMakeTemplate(filename string) (string, error) {
-	content, err := MakeFS.ReadFile("makes/" + filename)
-	if err != nil {
-		return "", fmt.Errorf("failed to read make template %s: %w", filename, err)
-	}
-	return string(content), nil
+type MakeData struct {
+    Prefix     string
+    ModuleName string
+}
+
+func RenderMakeTemplate(filename string, data MakeData) (string, error) {
+    path := "makes/" + filename
+    content, err := MakeFS.ReadFile(path)
+    if err != nil {
+        return "", fmt.Errorf("failed to read make template %s: %w", filename, err)
+    }
+
+    tmpl, err := template.New(filename).Parse(string(content))
+    if err != nil {
+        return "", fmt.Errorf("failed to parse make template %s: %w", filename, err)
+    }
+
+    var buf bytes.Buffer
+    if err := tmpl.Execute(&buf, data); err != nil {
+        return "", fmt.Errorf("failed to execute make template %s: %w", filename, err)
+    }
+
+    return buf.String(), nil
 }
